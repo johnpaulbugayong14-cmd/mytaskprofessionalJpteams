1daac1ce20eb5e3f614b14d2a0f869ee0994792d
John Paul Bugayong <johnpaulbugayong14@gmail.com>
2026-04-20 00:04:22 +0800

    Add files via upload

--- trigger-email.js
+++ trigger-email.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (req.method === 'OPTIONS') {
     return res.status(204).end();
